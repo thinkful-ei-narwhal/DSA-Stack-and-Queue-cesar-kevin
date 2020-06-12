3ac571170d7c7c6d8b5b216e9b537d4edca234d8
Kevin Jiang <kjiang95@gmail.com>
2020-06-12 10:54:00 -0500

    finished 2

--- drills.js
+++ drills.js
@@ -1,6 +1,30 @@
 const Stack = require('./stack');
 const Queue = require('./queue');
 
+const peek = (stack) => {
+  return stack.top;
+};
+
+const isEmpty = (stack) => {
+  if (stack.top === null) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const display = (stack) => {
+  let temp = stack.top;
+  if (temp === null) {
+    return 'nothing in stack';
+  } else {
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+};
 
 function main(){
   const starTrek = new Stack;
@@ -9,6 +33,10 @@ function main(){
   starTrek.push('Spock');
   starTrek.push('McCoy');
   starTrek.push('Scotty');
+
+  // console.log(peek(starTrek));
+  // console.log(isEmpty(starTrek));
+  console.log(display(starTrek));
 }
 
 const mainRun = main();
