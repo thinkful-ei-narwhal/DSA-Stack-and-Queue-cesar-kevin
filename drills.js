bb7cc70bc6a1349f849112ff31826c2b50bd3b92
Cesar Morales <cesarlenin2@live.com>
2020-06-12 08:35:18 -0700

    setting everything up

---
+++ drills.js
@@ -0,0 +1,12 @@
+const Stack = require('./stack');
+const Queue = require('./queue');
+
+
+function main(){
+  const SLL = new Stack;
+  const SLL2 = new Queue;
+  
+  console.log('ran');
+}
+
+const mainRun = main();
